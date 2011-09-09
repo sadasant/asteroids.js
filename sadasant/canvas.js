3dd0f9f499b6ee7eb017eda27a831b2c2c597d03
Daniel R <sadasant@gmail.com>
2011-09-09 06:59:41 +0000

    Started canvas codes, some objects sketched, rotations are not working yet...

--- sadasant/canvas.js
+++ sadasant/canvas.js
@@ -1,3 +1,104 @@
 /* canvas.js
+ * By Daniel R. (sadasant.com)
+ * for #jsve (groups.google.com/group/jsv)
+ *
  * roids.js's objects rendered with canvas
+ *
  */
+ 
+var canvas = (function(){ //
+  /* PRIVATE VARS */
+  var started = "You've started canvas.js",
+      _drawStack = [],
+      apply_draw = null;
+  /* PRIVATE METHODS */
+  getWindowSize = function() {
+    var winW = 630, winH = 460;
+    if (document.body && document.body.offsetWidth) {
+     winW = document.body.offsetWidth;
+     winH = document.body.offsetHeight;
+    }
+    if (document.compatMode=='CSS1Compat' &&
+        document.documentElement &&
+        document.documentElement.offsetWidth ) {
+     winW = document.documentElement.offsetWidth;
+     winH = document.documentElement.offsetHeight;
+    }
+    if (window.innerWidth && window.innerHeight) {
+     winW = window.innerWidth;
+     winH = window.innerHeight;
+    }
+    return {w:winW,h:winH};
+  };
+  function drawStack(){
+    var can = canvas.can,
+        con = canvas.con;
+    if (this.clear) {
+      this.draw(new this.Rect(0, 0, can.width, can.height, this.clear));
+    }
+    for (var i in _drawStack){
+      con.save();
+      _drawStack[i].draw(con);
+      con.restore();
+    }
+  }
+  /* PUBLIC METHODS */
+  function draw(obj){
+    _drawStack.push(obj);
+  }
+  /* MOTHERS */
+  /* SONS */
+  function Triangle(v,fill,stroke){
+    this.v = v || [[25,25],[105,25],[25,105]];
+    this.fill = fill || "rgba(150, 255, 0, 0.3)";
+    this.stroke = stroke || "#96FF00";
+    this.moves = [];
+    this.rotate = function(rad){
+      this.moves.push(function (con) {
+        console.debug(rad);
+        con.translate(this.v[0][0],this.v[0][1]);
+        con.rotate(rad); // not working
+      });
+    };
+    this.draw = function(con){
+      console.debug(this.stroke);
+      if (typeof(this.moves[0]) === "function") {
+        this.moves[0].call(this,con);
+        this.moves.shift();
+      }
+      con.fillStyle = this.fill;
+      con.strokeStyle = this.stroke;
+      con.beginPath();
+      con.moveTo(this.v[0][0],this.v[0][1]);
+      con.lineTo(this.v[1][0],this.v[1][1]);
+      con.lineTo(this.v[2][0],this.v[2][1]);
+      con.closePath();
+      con.stroke();
+      con.fill();
+    };
+  }
+  // GO GO GO!!!
+  function start(id,d,width,height){
+    this.can = document.getElementById(id || "canvas");
+    var win = getWindowSize();
+    this.can.width = win.w;
+    this.can.height = win.h;
+    this.center = {x:win.w/2,y:win.h/2};
+    this.con = this.can.getContext(d || "2d");
+    this.can.width = width || this.can.width;
+    this.can.height = height || this.can.height;
+    this.clear = "rgba(0, 0, 0, 0.05)";
+    this.interval = setInterval(drawStack,3333); // should be between 30 and 35
+    if (console && console.debug) console.debug(started);
+    return started;
+  }
+  /* PUBLIC */
+  return {
+    can: null, //canvas
+    con: null, //context
+    start: start,
+    draw: draw,
+    Triangle: Triangle
+  };
+}());
+ 
--- sadasant/roids.js
+++ sadasant/roids.js
@@ -1,3 +1,75 @@
 /* roids.js
+ * By Daniel R. (sadasant.com)
+ * for #jsve (groups.google.com/group/jsv)
+ *
  * Here will be all the math stuff and objects for asteroidsjs
+ *
  */
+
+/* Anonymous functions also hacked paypal.
+ * No, but they can return stuff that can handle runtime variables.
+ * Which are unreachable for the user (private variables!).
+ * They're also called lambdas.
+ */
+var roids = (function(){ //
+  // All this codes are just for pre-defining the object
+  // so here be hidden vars and methods
+  var started = "You've started roids.js";
+  
+  /* The Ship class.
+   */
+  function Ship(R){
+    if (!R) return;
+    var pos = R.center;
+    this.obj = new R.Triangle([[pos.x,pos.y],[pos.x-10,pos.y+15],[pos.x+10,pos.y+15]]);
+    R.draw(this.obj);
+  }
+  // This should be in a parent, not sure...
+  Ship.prototype = { //
+    turn: function(to){
+      console.debug("turning "+to);
+      var rad = (to === "left")? 2*Math.PI/3 : Math.PI/3;
+      this.obj.rotate(rad);
+    },
+    accelerator: function(much){
+      much = much || 30;
+      console.debug("Going forward with "+much+" grades of speed.");
+    },
+    brake: function(much){
+      much = much || -30;
+      console.debug("Going forward with "+much+" grades of speed.");
+    }
+  };
+
+  /* The start function
+   * "Where everything begins"
+   * The R argument will decide if we'll work with canvas or css
+   * for that, we must put the same functions and variables in both methods
+   */
+  function start(R){
+    this.R = R; // this should do the reference
+    this.R.start(); // setting the renderer
+    this.hero = new Ship(R); // or this.Ship
+    // keyboarding!!!
+    document.addEventListener("keydown",function (e) {
+      switch (e.charCode || e.keyCode){ //
+        // turning
+        case 37:roids.hero.turn("left");  break;
+        case 39:roids.hero.turn("right"); break;
+        // accelerator
+        case 38:roids.hero.accelerator(); break;
+        // brake
+        case 40:roids.hero.brake(); break;
+      }
+    },true);
+    if (console && console.debug) console.debug(started);
+    return started;
+  }
+
+  // This is the real object
+  return {
+    R: null, // here will be stored the rendering method
+    hero: null, // our hero (Ship object)
+    start: start // Go go go!
+  };
+}());
