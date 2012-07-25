6e1095ca18c6610f07b9280b7481b99235b247a1
sadasant <sadasant@gmail.com>
2012-07-25 16:11:46 -0430

    last Ink.js and now the game is more playable

--- js/asteroids.js
+++ js/asteroids.js
@@ -11,15 +11,18 @@
     , U   // undefined
 
   // Private Variables
-  var ids         = 0
-    , level       = 0
-    , PI2         = M.PI*1.99
-    , ship_path   = [[0,-10],[-10,0],[10,0]]
-    , listeners   = false
-    , rocks       = []
-    , hero        // [Object Ship]
-    , started     = 0
+  var ids          = 0
+    , level        = 0
+    , PI2          = M.PI*1.99
+    , ship_path    = [[0,-10],[-10,0],[10,0]]
+    , listeners    = false
+    , rocks        = []
+    , hero         // [Object Ship]
+    , started      = 0
     , level_factor = 3
+    , level_speed_factor = 15
+    //, level_rocks_factor = 6
+    , frame        = 31
 
   // Avoid creating new functions every time
 
@@ -43,7 +46,7 @@
   , clearIntervals : function() {
       var i = 0
         , l = this.intervals.length
-      for (; i < l; i+=1) {
+      for (; i < l; i += 1) {
         clearInterval(this.intervals[i])
       }
     }
@@ -51,7 +54,7 @@
   , rotate : function(rad, hold) {
       if (hold) {
         clearInterval(this.intervals.rotate)
-        this.intervals.rotate = setInterval(rotation_interval.bind(this, rad), Ink.frame)
+        this.intervals.rotate = setInterval(rotation_interval.bind(this, rad), frame)
       } else {
         this.obj.rotation += rad
       }
@@ -60,7 +63,7 @@
   , accel : function(much, hold) {
       if (hold) {
         clearInterval(this.intervals.accel)
-        this.intervals.accel = setInterval(acceleration_interval.bind(this, much), Ink.frame)
+        this.intervals.accel = setInterval(acceleration_interval.bind(this, much), frame)
       } else {
         this.obj.accel(much)
       }
@@ -85,16 +88,30 @@
   // The Ship class
   function Ship() {
     var s = Object.create(Main)
-    s.id                = (ids+=1).toString()
+      , shot_path
+    s.id                = (ids += 1).toString()
     s.alive             = true
     s.busy              = false
     s.fill              = "rgba(150, 255, 0, 0.3)"
-    s.stroke            = "rgba(150, 255, 0, 1  )"
-    s.obj               = new Ink.Path(Ink.center.x, Ink.center.y, ship_path, s.fill, s.stroke)
+    s.border_color      = "rgba(150, 255, 0, 1  )"
+    s.obj               = new Ink.Path({
+      x : Ink.center.x
+    , y : Ink.center.y
+    , path : ship_path
+    , fill : s.fill
+    , border_color : s.border_color
+    })
     s.obj.maxSpeed      = { x: 2, y: 2}
     s.obj.infiniteScope = true
     s.obj.onCollide     = Ship_onCollide.bind(s)
     s.shots             = []
+    shot_path = {
+      x : 0
+    , y : 0
+    , r : 1
+    , fill : s.fill
+    , border_color : s.border_color
+    }
 
     Ink.draw(s.obj)
 
@@ -103,22 +120,21 @@
       // Not alive? Busy? Don't shoot.
       if (!s.alive || s.busy) return
 
-      var len  = s.shots.length
-        , shot = new Ink.Circ(s.obj.x, s.obj.y, 1, s.fill, s.stroke)
+      var shots = s.shots.length
+        , shot
         , i, l
+
+      shot_path.x = s.obj.x
+      shot_path.y = s.obj.y
+      shot = new Ink.Circ(shot_path)
+      shot.shot_position = shots
 
       // On collide, remove the shot and trigger
       // the encountered's collision
-      shot.onCollide = function(obj) {
-        if (obj !== U && typeof obj.onCollide === 'function') {
-          obj.onCollide()
-        }
-        Ink.remove(s.shots[len])
-        s.shots[len] = null
-      }
+      shot.onCollide = shotOnCollide
 
       // The shoots will last according to the level
-      setTimeout(shot.onCollide, 500 * level)
+      setTimeout(shotOnCollide.bind(shot), 300 * level)
 
       shot.infiniteScope = true
       shot.rotation      = this.obj.rotation
@@ -132,22 +148,33 @@
 
       // Limit shoot frecuency
       // according to the ship's level
-      setTimeout(function() {
-        s.busy = false
-      }, 500 - level * 50)
+      setTimeout(allowNextShot, 500 - level * 60)
 
       // The shoot will collide with it's shooter
-      setTimeout(function() {
-        var shot = s.shots[len]
-        if (shot) shot.addCollider(hero.obj)
-      }, 500)
+      setTimeout(addHeroToShotColliders.bind(shot), 500)
 
       // The shoot will collide with rocks
       i = 0
       l = rocks.length
-      for (; i < l; i+=1) {
-        s.shots[len].addCollider(rocks[i].obj)
+      for (; i < l; i += 1) {
+        s.shots[shots].addCollider(rocks[i].obj)
       }
+    }
+
+    function shotOnCollide(obj) {
+      if (obj !== U && typeof obj.onCollide === 'function') {
+        obj.onCollide()
+      }
+      Ink.remove(s.shots[this.shot_position])
+      s.shots[this.shot_position] = null
+    }
+
+    function allowNextShot() {
+      s.busy = false
+    }
+
+    function addHeroToShotColliders() {
+      this.addCollider(hero.obj)
     }
 
     return s // this
@@ -174,7 +201,7 @@
       i = 0
       l = hero.shots.length
       setTimeout(function shotSensitive(rock1, rock2) {
-        for (; i < l; i+=1) {
+        for (; i < l; i += 1) {
           shot = hero.shots[i]
           if (shot) {
             rock1.obj.addCollider(shot)
@@ -183,7 +210,7 @@
             shot.addCollider(rock2.obj)
           }
         }
-      }.bind(null,rock1, rock2), Ink.frame * 10)
+      }.bind(null,rock1, rock2), frame * 10)
       updateRockColliders(1500)
     }
     Ink.remove(this.obj)
@@ -194,13 +221,19 @@
     var s = Object.create(Main)
       , path            = [[0,-size*10],[-size*10,0],[-size*10/2,size*10],[size*10,0]]
       , fill            = "rgba(82, 163, 0, 0.3)"
-      , stroke          = "rgba(82, 163, 0, 1  )"
+      , border_color    = "rgba(82, 163, 0, 1  )"
       , posx            = x || (random ? Ink.can.width  * M.random() << 0 : Ink.center.x)
       , posy            = y || (random ? Ink.can.height * M.random() << 0 : Ink.center.y)
-    s.id                = (ids+=1).toString()
+    s.id                = (ids += 1).toString()
     s.colliders         = []
     s.level             = size || 2
-    s.obj               = new Ink.Path(posx, posy, path, fill, stroke)
+    s.obj               = new Ink.Path({
+      x : posx
+    , y : posy
+    , path : path
+    , fill : fill
+    , border_color : border_color
+    })
     s.obj.maxSpeed      = { x: 7, y: 7 }
     s.obj.infiniteScope = true
     s.intervals         = []
@@ -226,14 +259,14 @@
           obj.rotate(rotate)
           obj.accel(accel)
         }
-     })(s), Ink.frame))
+     })(s), frame))
     }
 
     // updateColliders
     s.updateColliders = function() {
       var i = 0
         , l = rocks.length
-      for (; i < l; i+=1){
+      for (; i < l; i += 1){
         var id = rocks[i].id
         if (id !== s.id && !~s.colliders.indexOf(id)) {
           s.colliders.push(id)
@@ -250,34 +283,34 @@
     var i = 0
       , l = rocks.length
     setTimeout(function() {
-      for (; i < l; i+=1) {
+      for (; i < l; i += 1) {
         rocks[i] && rocks[i].updateColliders()
       }
     }, t)
   }
 
   function levelRocks(){
-    level+=1
-    var level_speed = level/level_factor
-      , lev         = M.ceil(level/level_factor)
+    level += 1
+    var level_speed = level/(level_speed_factor)
+      //, level_rocks = M.ceil(level/level_rocks_factor)
       , i = 0
+      , j = 0
+      , shots = hero.shots.length
       , rock
       , s
     hero.obj.maxSpeed.x += level_speed
     hero.obj.maxSpeed.y += level_speed
     doc.getElementById("level").innerHTML = level - 1
-    levels.push(lev)
-    for (; i < lev; i+=1) {
-      rocks.push(new Rock(level_factor, true))
-      rock = rocks[rocks.length - 1]
-      rock.inEdge()
-      rock.randomize()
-      hero.obj.addCollider(rock.obj)
-      for (s in hero.shots) {
-        if (hero.shots[s] !== null) {
-          rock.obj.addCollider(hero.shots[s])
-          hero.shots[s].addCollider(rock.obj)
-        }
+    levels[levels.length] = 1
+    rocks.push(new Rock(level_factor, true))
+    rock = rocks[rocks.length - 1]
+    rock.inEdge()
+    rock.randomize()
+    hero.obj.addCollider(rock.obj)
+    for (; j !== shots; j += 1) {
+      if (hero.shots[j] !== null) {
+        rock.obj.addCollider(hero.shots[j])
+        hero.shots[j].addCollider(rock.obj)
       }
     }
     updateRockColliders()
@@ -331,13 +364,12 @@
     rocks.map(function(r) { r.clearIntervals() })
     hero  = null
     rocks = []
-    Ink.init(U, U, U, "rgba(0, 0, 0, .2)") // Starting Ink.js
+    Ink.init({ clear : "rgba(0, 0, 0, .2)" }) // Starting Ink.js
     hero = new Ship()
-    /**/
     // Uncomment the 3 lines below and comment the 2 lines above
     // to test a more traditional asteroids game
     /*
-    Ink.init(U, U, U, "rgba(0, 0, 0, 1)") // Starting Ink.js
+    Ink.init({ clear : "rgba(0, 0, 0, 1)" }) // Starting Ink.js
     hero = new Ship()
     hero.obj.rotateInEdge  = true
     */
